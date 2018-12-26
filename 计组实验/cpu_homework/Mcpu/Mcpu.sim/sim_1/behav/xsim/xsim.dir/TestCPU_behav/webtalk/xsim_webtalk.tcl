28c276d1a84b00accbcad1e6b10e23ea56b16e14
quanao <862238910@qq.com>
2018-12-26 21:52:28 +0800

    计组实验作业

--- 计组实验/cpu_homework/Mcpu/Mcpu.sim/sim_1/behav/xsim/xsim.dir/TestCPU_behav/webtalk/xsim_webtalk.tcl
+++ 计组实验/cpu_homework/Mcpu/Mcpu.sim/sim_1/behav/xsim/xsim.dir/TestCPU_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir G:/cpu_homework/Mcpu/Mcpu.sim/sim_1/behav/xsim/xsim.dir/TestCPU_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Wed Dec 26 18:37:28 2018" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Wed Dec 26 20:53:14 2018" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2017.4 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2086221" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "7e3ef37b-5e0a-4c10-b9fb-46144ab00fae" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "b34567b01e044468910bb09303ff5162" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "140" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "145" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Microsoft Windows 8 or later , 64-bit" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i7-7700HQ CPU @ 2.80GHz" -context "user_environment"
@@ -24,9 +24,9 @@ webtalk_add_data -client project -key system_ram -value "8.000 GB" -context "use
 webtalk_register_client -client xsim
 webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
-webtalk_add_data -client xsim -key runtime -value "21 us" -context "xsim\\usage"
-webtalk_add_data -client xsim -key iteration -value "4" -context "xsim\\usage"
+webtalk_add_data -client xsim -key runtime -value "1 us" -context "xsim\\usage"
+webtalk_add_data -client xsim -key iteration -value "6" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Simulation_Time -value "0.17_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "7440_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 3821726488 -regid "" -xml G:/cpu_homework/Mcpu/Mcpu.sim/sim_1/behav/xsim/xsim.dir/TestCPU_behav/webtalk/usage_statistics_ext_xsim.xml -html G:/cpu_homework/Mcpu/Mcpu.sim/sim_1/behav/xsim/xsim.dir/TestCPU_behav/webtalk/usage_statistics_ext_xsim.html -wdm G:/cpu_homework/Mcpu/Mcpu.sim/sim_1/behav/xsim/xsim.dir/TestCPU_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Memory -value "7088_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 653847447 -regid "" -xml G:/cpu_homework/Mcpu/Mcpu.sim/sim_1/behav/xsim/xsim.dir/TestCPU_behav/webtalk/usage_statistics_ext_xsim.xml -html G:/cpu_homework/Mcpu/Mcpu.sim/sim_1/behav/xsim/xsim.dir/TestCPU_behav/webtalk/usage_statistics_ext_xsim.html -wdm G:/cpu_homework/Mcpu/Mcpu.sim/sim_1/behav/xsim/xsim.dir/TestCPU_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
